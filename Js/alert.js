f21fd3bf5c1f0d743218ad6c5477b3126673b1d0
De Boy programmer <ciscomahlee@gmail.com>
2025-04-04 02:57:34 +0100

    Minor Change

--- Js/alert.js
+++ Js/alert.js
@@ -1,3 +1,22 @@
+// SIDEBAR TOGGLE
+
+let sidebarOpen = false;
+const sidebar = document.getElementById('sidebar');
+
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add('sidebar-responsive');
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove('sidebar-responsive');
+    sidebarOpen = false;
+  }
+}
+// End of SideBar Toggle
 const INVENTORY_ALERT_THRESHOLD = 1000; // Define inventory alert threshold
 
 // Function to render product data and track inventory alerts
